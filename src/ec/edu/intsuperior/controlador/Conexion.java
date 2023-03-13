0a13ffea86be56fdc54e61afb288e4fcfccf893c
jefers17 <lenin.puetate@intsuperior.edu.ec>
2023-03-13 16:45:22 -0500

    Primera version de proyecto final

---
+++ src/ec/edu/intsuperior/controlador/Conexion.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.controlador;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author user
+ */
+public class Conexion {
+    Controlador controlador;
+
+    public Conexion(Controlador controlador) {
+        this.controlador=controlador;
+    }
+    
+   static Connection conex=null;
+    
+    public static Connection conexion(){
+        String user="root";
+        String passwd="f2m32831ao";
+        String url="jdbc:mysql://localhost:3306/bimal?serverTimezone=UTC";
+        try {
+            conex=DriverManager.getConnection(url,user,passwd);
+            System.out.println("Conexion Exitosa");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return conex;
+    }
+    
+    public void consultarUsuario(){
+       try {
+           Statement stm= conexion().createStatement();
+           ResultSet rst=stm.executeQuery("select * from usuario");
+           while (rst.next()){
+               System.out.println("Id User: " + rst.getInt(1)
+                        + "\nUser Name: " + rst.getString(2)
+                        + "\nPassword: " + rst.getString(3));
+                System.out.println("**********************************");
+           }
+       } catch (SQLException ex) {
+           Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+       }
+        
+    }
+    
+}
